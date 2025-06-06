9916c41684f8a76cf3b34e77513135ffcc100f10
Md. Pieash Ahmed <pieash9@gmail.com>
2025-06-06 08:31:21 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/ai-image-generate/",
+  base: "/ai-image-generater/",
 });
